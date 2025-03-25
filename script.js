4e0b5e52aa2bab0a32eadd727363ab254582f651
TheOrangeCow <138312355+TheOrangeCow@users.noreply.github.com>
2025-03-25 18:05:12 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 if (window.top !== window.self) {
+    async function isWebsiteAllowed() {
+        try {
+            const response = await fetch('https://778.house-778.org/dev/list.json');
+            if (!response.ok) {
+                throw new Error('Failed to fetch JSON');
+            }
+            const data = await response.json();
+            
+            
+            const topWebsite = new URL(document.referrer).hostname;
+            
+            
+            return data.allowedWebsites.includes(topWebsite);
+        } catch (error) {
+            console.error('Error:', error);
+            return false;
+        }
+    }
+
+    isWebsiteAllowed().then(isAllowed => {
+        console.log('Is website allowed:', isAllowed);
+        if (!isAllowed) {
+            document.body.innerHTML = "<h1>go away</h1>";
+        }
+    });
+}
+
+
+/*if (window.top !== window.self) {
   
   document.body.innerHTML = "<h1>go away</h1>";
-}
+}*/
